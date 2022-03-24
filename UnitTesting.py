fb55bfbf7eab78f3a5375a9c45cb3ee9f7ffb8cc
unknown <prathmeshsandikar@gmail.com>
2022-03-24 16:36:15 +0530

    Testing of divisible by7 or not

--- UnitTesting.py
+++ UnitTesting.py
@@ -5,6 +5,22 @@ def add(x,y):
 
     return x+y
 
+def checkDivisibleby7(x):
+    if x%7 == 0:
+        return True
+    else:
+        return False
+class CheckDivisible(unittest.TestCase):
+    def test_case_divisible(self):
+        x=14
+        result = checkDivisibleby7(x)
+        self.assertTrue(result)
+
+    def test_case_not_divisible(self):
+        x=9
+        result = checkDivisibleby7(x)
+        self.assertFalse(result)
+
 
 
 
